d38ca7a911bc473f0ceab373d905d28e74d59d16
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-05-29 01:08:58 +0700

    fix ui

--- app/src/main/java/com/nqmgaming/documentscanner/MainActivity.kt
+++ app/src/main/java/com/nqmgaming/documentscanner/MainActivity.kt
@@ -14,7 +14,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -39,7 +42,9 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 import java.io.File
@@ -60,64 +65,64 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             DocumentScannerTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                var images by remember {
+                    mutableStateOf<List<Uri>>(emptyList())
+                }
 
-                    var images by remember {
-                        mutableStateOf<List<Uri>>(emptyList())
-                    }
+                val scannerLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartIntentSenderForResult(),
+                    onResult = { activityResult ->
+                        if (activityResult.resultCode == RESULT_OK) {
+                            val result = GmsDocumentScanningResult.fromActivityResultIntent(
+                                activityResult.data!!
+                            )
+                            images = (result?.pages?.map {
+                                it.imageUri
+                            } ?: emptyList())
 
-                    val scannerLauncher = rememberLauncherForActivityResult(
-                        contract = ActivityResultContracts.StartIntentSenderForResult(),
-                        onResult = { activityResult ->
-                            if (activityResult.resultCode == RESULT_OK) {
-                                val result = GmsDocumentScanningResult.fromActivityResultIntent(
-                                    activityResult.data!!
-                                )
-                                images = (result?.pages?.map {
-                                    it.imageUri
-                                } ?: emptyList())
+                            // Save the images to the app's directory
+                            val files = images.map { uri ->
+                                val file = File(filesDir, uri.lastPathSegment!!)
+                                val inputStream = contentResolver.openInputStream(uri)
+                                val outputStream = FileOutputStream(file)
+                                inputStream?.copyTo(outputStream)
+                                file
+                            }
 
-                                // Save the images to the app's directory
-                                val files = images.map { uri ->
-                                    val file = File(filesDir, uri.lastPathSegment!!)
-                                    val inputStream = contentResolver.openInputStream(uri)
-                                    val outputStream = FileOutputStream(file)
-                                    inputStream?.copyTo(outputStream)
-                                    file
+                            contentResolver.openInputStream(result?.pdf?.uri!!)
+                                ?.use { inputStream ->
+                                    val outputStream =
+                                        FileOutputStream(File(filesDir, "document.pdf"))
+                                    inputStream.copyTo(outputStream)
                                 }
 
-                                contentResolver.openInputStream(result?.pdf?.uri!!)
-                                    ?.use { inputStream ->
-                                        val outputStream =
-                                            FileOutputStream(File(filesDir, "document.pdf"))
-                                        inputStream.copyTo(outputStream)
-                                    }
-
-                            }
                         }
-                    )
-
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        images.forEach { image ->
-                            AsyncImage(
-                                model = image,
-                                contentDescription = null,
-                                contentScale = ContentScale.FillWidth,
+                    }
+                )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        Surface(
+                            color = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Document Scanner",
+                                style = MaterialTheme.typography.labelLarge.copy(
+                                    fontSize = 20.sp
+                                ),
                                 modifier = Modifier
+                                    .padding(16.dp)
                                     .fillMaxWidth()
-                                    .padding(vertical = 8.dp)
+                                    .padding(top = 10.dp),
+                                textAlign = TextAlign.Center
                             )
                         }
-                        Button(
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(
                             onClick = {
                                 scanner.getStartScanIntent(this@MainActivity)
                                     .addOnSuccessListener {
@@ -135,11 +140,36 @@ class MainActivity : ComponentActivity() {
                                             Toast.LENGTH_SHORT
                                         ).show()
                                     }
-                            },
-                            modifier = Modifier.padding(16.dp)
+                            }
                         ) {
-                            Text("Scan Document")
+                            Text("Scan")
                         }
+                    }
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .verticalScroll(rememberScrollState())
+                        ,
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        images.forEach { image ->
+                            AsyncImage(
+                                model = image,
+                                contentDescription = null,
+                                contentScale = ContentScale.FillWidth,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 8.dp, horizontal = 16.dp)
+                            )
+                        }
+
+                        if (images.isEmpty()) {
+                            Text("No images to display")
+                        }
+
                     }
                 }
             }
